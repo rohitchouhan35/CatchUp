a9aa5de13eb779acf780a91bf08dbfe3cea76543
rohitchouhan3512 <rohitchouhan3512@gmail.com>
2023-12-31 15:37:21 +0530

    create: Avatar.jsx for serving random profile images

---
+++ client/src/components/Avatars.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Avatar, { genConfig } from 'react-nice-avatar'
+
+const Avatars = () => {
+    // const config = genConfig("hi@dapi.to");
+    // const config = genConfig({ sex: "man", hairStyle: "mohawk" });
+    const config = genConfig();
+  return (
+    <div className='random-avatar'>
+      <Avatar style={{ width: '8rem', height: '8rem' }} {...config} />
+    </div>
+  )
+}
+
+export default Avatars
